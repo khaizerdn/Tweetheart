620e868944f1d3afd0e95ebb8a23dd9b1439cd13
khaizerdn <khaizerdn@gmail.com>
2025-10-27 21:17:09 +0800

    Update: rightContainers is centered starting 1279px and below

--- src/features/SignUp/index.jsx
+++ src/features/SignUp/index.jsx
@@ -256,8 +256,8 @@ function SignUp() {
       {/* Right Container - Input Fields and Photos */}
       <div className={styles.rightContainer}>
         <div className={styles.formSection}>
-          <h1>Sign Up</h1>
           <form onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
             <div className={styles.formContent}>
               {/* Input Fields Section */}
               <div className={styles.inputFieldsSection}>
